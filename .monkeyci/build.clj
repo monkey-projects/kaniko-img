d6d1f5647c8a32ab2ed638cdfcab630011d9abd0
Wout Neirynck <wout@monkey-projects.be>
2024-03-09 11:32:12 +0100

    And another fix

--- .monkeyci/build.clj
+++ .monkeyci/build.clj
@@ -39,10 +39,10 @@
      "build-image"
      {:image (str image ":" build-version)
       :script [(cs/join " "
-                        "/kaniko/executor"
-                        ;; TODO Replace with shell/container-work-dir when it becomes available
-                        "--context" (str "dir://" wd)
-                        "--destination" (str image ":" release-version))]
+                        ["/kaniko/executor"
+                         ;; TODO Replace with shell/container-work-dir when it becomes available
+                         "--context" (str "dir://" wd)
+                         "--destination" (str image ":" release-version)])]
       :container/env {"DOCKER_CONFIG" (str wd (:path docker-creds))}
       :dependencies ["generate-docker-creds"]
       :restore-artifacts [docker-creds]})))
